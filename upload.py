9f46802883cf1a5da2140df31dc6f7a9bcb6c3a7
hw@chill <hw56@indiana.edu>
2024-05-28 13:00:50 -0400

    feat: add upload to hf funcs

--- upload.py
+++ upload.py
@@ -4,25 +4,25 @@ import shutil
 from huggingface_hub import HfApi, Repository
 
 
-def upload_model_to_hf(hf_username, hf_repo_name, model_checkpoint_dir):
+def upload_model_to_hf(username, repo_name, ckpt_dir):
     # create a new repository on Hugging Face Hub
     api = HfApi()
-    api.create_repo(repo_id=f"{hf_username}/{hf_repo_name}", private=False)
+    api.create_repo(repo_id=f"{username}/{repo_name}", private=False)
 
     # clone the repository
-    repo = Repository(local_dir=f"./{hf_repo_name}",
-                      clone_from=f"{hf_username}/{hf_repo_name}")
+    repo = Repository(local_dir=f"./{repo_name}",
+                      clone_from=f"{username}/{repo_name}")
 
     # copy your model files to the repository directory
-    for file_name in os.listdir(model_checkpoint_dir):
-        full_file_name = os.path.join(model_checkpoint_dir, file_name)
+    for file_name in os.listdir(ckpt_dir):
+        full_file_name = os.path.join(ckpt_dir, file_name)
         if os.path.isfile(full_file_name):
             shutil.copy(full_file_name, repo.local_dir)
 
     # push the files to the repository
     repo.push_to_hub()
 
-    print(f"Model {hf_repo_name} has been successfully uploaded to Hugging Face Hub.")
+    print(f"Model {repo_name} has been successfully uploaded to Hugging Face Hub.")
 
 
 if __name__ == "__main__":
@@ -32,9 +32,9 @@ if __name__ == "__main__":
                         help="The name of the Hugging Face repository.")
     parser.add_argument('--ckpt_dir', type=str, required=True,
                         help="The directory of the model checkpoint.")
-    parser.add_argument('--hf_username', type=str, default="haining",
+    parser.add_argument('--username', type=str, default="haining",
                         help="Your Hugging Face username.")
 
     args = parser.parse_args()
 
-    upload_model_to_hf(args.hf_username, args.hf_repo_name, args.model_checkpoint_dir)
+    upload_model_to_hf(args.username, args.repo_name, args.ckpt_dir)
